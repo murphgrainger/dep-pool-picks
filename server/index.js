1740f492aab79e900c1e0912f0ac0ec3e171254c
Murph Grainger <murphgrainger@Murphs-Macbook.local>
2022-04-12 17:49:25 -0600

    added server and rearranged file structure

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static(__dirname));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build.index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+})
